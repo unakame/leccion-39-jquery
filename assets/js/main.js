644b9b24285dea3794b8d544966ada4c9e394c30
Dana Franco <turquoise.ilg@hotmail.com>
2017-06-01 16:02:08 -0500

    testing jquery

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+//var jq = jQuery.noConflict();
+// Cambia el simbolo del dolar por jq para qe no haga conflicto
+// con otras librerias qe taqmbien usen el dolar
+
+/*var cb = function(){
+  alert ("Cargo jQuery");
+}
+$(document).ready(cb);*/
+
+// otra formar de hacer el Load
+
+/*$(document).ready(function(){
+  alert ("Cargo jQuery");
+});*/
+
+// otra forma
+
+/*$(function(){
+  alert ("Cargo jQuery");
+})*/
+
+//otra
+
+/*$(() => { //function {}
+  alert ("cargo jQuery");
+})*/
+
+//otra
+
+$( _ => { //function {}
+  alert ("cargo jQuery");
+})
+
+/*(()=> a{ => u {
+  alert ("dd")
+})} algo raro salio mal aca*/
